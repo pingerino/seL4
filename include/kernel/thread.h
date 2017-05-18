7c28f49ec56da44f16eaf417c33fb73f89a8901d
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-05-18 12:17:26 +1000

    VER-781: Avoid rolling back time if acted upon
    Otherwise strange things can happen where threads are in the future.

--- include/kernel/thread.h
+++ include/kernel/thread.h
@@ -105,6 +105,10 @@ commitTime(void)
 static inline void
 rollbackTime(void)
 {
+    /* it is invalid to rollback time if we
+     * have already acted on the new time */
+    assert(!NODE_STATE(ksReprogram) || NODE_STATE(ksConsumed) == 0);
+
     NODE_STATE(ksCurTime) -= NODE_STATE(ksConsumed);
     NODE_STATE(ksConsumed) = 0llu;
 }
@@ -201,7 +205,7 @@ checkBudget(void)
 	if (likely(capacity >= MIN_BUDGET && (isRoundRobin(NODE_STATE(ksCurSC)) ||
                     !refill_full(NODE_STATE(ksCurSC))))) {
         if (unlikely(isCurDomainExpired())) {
-            commitTime();
+            NODE_STATE(ksReprogram) = true;
             rescheduleRequired();
             return false;
         }
