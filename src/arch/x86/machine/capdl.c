e8df5079646429f00744e286e930cc90e7c08893
Anna Lyons <anna@gh.st>
2019-06-26 17:07:40 +1000

    capdl: remove general code from ia32 capdl impl
    Pull out code made redundant by the previous commit from the ia32
    implementation of capdl output.

--- src/arch/x86/machine/capdl.c
+++ src/arch/x86/machine/capdl.c
@@ -18,106 +18,10 @@
 
 #ifdef CONFIG_DEBUG_BUILD
 
-#define ARCH 0xe1
-
 #define PD_READ_SIZE         BIT(PD_INDEX_BITS)
 #define PT_READ_SIZE         BIT(PT_INDEX_BITS)
 #define ASID_POOL_READ_SIZE  BIT(ASID_POOL_INDEX_BITS)
 #define IO_PT_READ_SIZE      BIT(VTD_PT_INDEX_BITS)
-
-static int getDecodedChar(unsigned char *result)
-{
-    unsigned char c;
-    c = getDebugChar();
-    if (c == START) {
-        return 1;
-    }
-    if (c == ESCAPE) {
-        c = getDebugChar();
-        if (c == START) {
-            return 1;
-        }
-        switch (c) {
-        case ESCAPE_ESCAPE:
-            *result = ESCAPE;
-            break;
-        case START_ESCAPE:
-            *result = START;
-            break;
-        case END_ESCAPE:
-            *result = END;
-            break;
-        default:
-            if (c >= 20 && c < 40) {
-                *result = c - 20;
-            } else {
-                return 1;
-            }
-        }
-        return 0;
-    } else {
-        *result = c;
-        return 0;
-    }
-}
-
-static void putEncodedChar(unsigned char c)
-{
-    switch (c) {
-    case ESCAPE:
-        putDebugChar(ESCAPE);
-        putDebugChar(ESCAPE_ESCAPE);
-        break;
-    case START:
-        putDebugChar(ESCAPE);
-        putDebugChar(START_ESCAPE);
-        break;
-    case END:
-        putDebugChar(ESCAPE);
-        putDebugChar(END_ESCAPE);
-        break;
-    default:
-        if (c < 20) {
-            putDebugChar(ESCAPE);
-            putDebugChar(c + 20);
-        } else {
-            putDebugChar(c);
-        }
-    }
-}
-
-static int getArg(unsigned long *res)
-{
-    unsigned long i;
-    unsigned char byte;
-    *res = 0;
-    for (i = 0; i < sizeof(unsigned long); i++) {
-        if (getDecodedChar(&byte)) {
-            return 1;
-        }
-        (*res) = ((*res) << 8) | byte;
-    }
-    return 0;
-}
-
-static void sendWord(unsigned long word)
-{
-    unsigned long i;
-    for (i = 0; i < sizeof(unsigned long); i++) {
-        putEncodedChar((word >> (i * 8)) & 0xff);
-    }
-}
-
-static cte_t *getMDBParent(cte_t *slot)
-{
-    cte_t *oldSlot = CTE_PTR(mdb_node_get_mdbPrev(slot->cteMDBNode));
-
-    while (oldSlot != 0 && !isMDBParentOf(oldSlot, slot)) {
-        oldSlot = CTE_PTR(mdb_node_get_mdbPrev(oldSlot->cteMDBNode));
-    }
-
-    return oldSlot;
-}
 
 static void sendPD(unsigned long address)
 {
@@ -212,194 +116,46 @@ static void sendIOSpace(uint32_t pci_request_id)
 }
 #endif
 
-static void sendRunqueues(void)
+int doArchCommand(unsigned char c, word_t arg)
 {
-    word_t i;
-    sendWord((unsigned long)NODE_STATE(ksCurThread));
-    for (i = 0; i < NUM_READY_QUEUES; i++) {
-        tcb_t *current = NODE_STATE(ksReadyQueues[i]).head;
-        if (current != 0) {
-            while (current != NODE_STATE(ksReadyQueues[i]).end) {
-                sendWord((unsigned long)current);
-                current = current -> tcbSchedNext;
-            }
-            sendWord((unsigned long)current);
-        }
-    }
-}
-
-static void sendEPQueue(unsigned long epptr)
-{
-    tcb_t *current = (tcb_t *)endpoint_ptr_get_epQueue_head((endpoint_t *)epptr);
-    tcb_t *tail = (tcb_t *)endpoint_ptr_get_epQueue_tail((endpoint_t *)epptr);
-    if (current == 0) {
-        return;
-    }
-    while (current != tail) {
-        sendWord((unsigned long)current);
-        current = current->tcbEPNext;
-    }
-    sendWord((unsigned long)current);
-}
-
-static void sendCNode(unsigned long address, unsigned int sizebits)
-{
-    unsigned long i;
-    cte_t *start = (cte_t *)address;
-    for (i = 0; i < (1 << sizebits); i++) {
-        cap_t cap = start[i].cap;
-        if (cap_get_capType(cap) != cap_null_cap) {
-            cte_t *parent = getMDBParent(&start[i]);
-            sendWord(i);
-            sendWord(cap.words[0]);
-            sendWord(cap.words[1]);
-            sendWord((unsigned long)parent);
-        }
-    }
-}
-
-static void sendIRQNode(void)
-{
-    sendCNode((unsigned long)intStateIRQNode, 8);
-}
-
-static void sendVersion(void)
-{
-    sendWord(ARCH);
-    sendWord(CAPDL_VERSION);
-}
-
-void capDL(void)
-{
-    int result;
-    int done = 0;
-    while (done == 0) {
-        unsigned char c;
-        do {
-            c = getDebugChar();
-        } while (c != START);
-        do {
-            result = getDecodedChar(&c);
-            if (result) {
-                continue;
-            }
-            switch (c) {
-            case PD_COMMAND: {
-                /*pgdir */
-                unsigned long arg;
-                result = getArg(&arg);
-                if (result) {
-                    continue;
-                }
-                sendPD(arg);
-                putDebugChar(END);
-            }
-            break;
-            case PT_COMMAND: {
-                /*pg table */
-                unsigned long arg;
-                result = getArg(&arg);
-                if (result) {
-                    continue;
-                }
-                sendPT(arg);
-                putDebugChar(END);
-            }
-            break;
-            case ASID_POOL_COMMAND: {
-                /*asid pool */
-                unsigned long arg;
-                result = getArg(&arg);
-                if (result) {
-                    continue;
-                }
-                sendASIDPool(arg);
-                putDebugChar(END);
-            }
-            break;
+    switch (c) {
+    case PD_COMMAND:
+        /*pgdir */
+        sendPD(arg);
+        putDebugChar(END);
+        break;
+    case PT_COMMAND:
+        /*pg table */
+        sendPT(arg);
+        putDebugChar(END);
+        break;
+    case ASID_POOL_COMMAND:
+        /*asid pool */
+        sendASIDPool(arg);
+        putDebugChar(END);
+        break;
 #ifdef CONFIG_IOMMU
-            case IO_PT_COMMAND: {
-                /*io pt table */
-                unsigned long address, level;
-                result = getArg(&address);
-                if (result) {
-                    continue;
-                }
-                result = getArg(&level);
-                if (result) {
-                    continue;
-                }
-                sendIOPT(address, level);
-                putDebugChar(END);
+    case IO_PT_COMMAND: {
+        /*io pt table */
+        unsigned long address = arg;
+        word_t result = getArg32(&level);
+        if (result) {
+            return result;
             }
-            break;
-            case IO_SPACE_COMMAND: {
-                /*io space */
-                unsigned long arg;
-                result = getArg(&arg);
-                if (result) {
-                    continue;
-                }
-                sendIOSpace(arg);
-                putDebugChar(END);
-            }
+            sendIOPT(address, level);
+            putDebugChar(END);
+        }
+        break;
+    case IO_SPACE_COMMAND:
+        /*io space */
+        sendIOSpace(arg);
+        putDebugChar(END);
+        break;
+    default:
+        return 0;
 #endif
-            break;
-            case RQ_COMMAND: {
-                /*runqueues */
-                sendRunqueues();
-                putDebugChar(END);
-                result = 0;
-            }
-            break;
-            case EP_COMMAND: {
-                /*endpoint waiters */
-                unsigned long arg;
-                result = getArg(&arg);
-                if (result) {
-                    continue;
-                }
-                sendEPQueue(arg);
-                putDebugChar(END);
-            }
-            break;
-            case CN_COMMAND: {
-                /*cnode */
-                unsigned long address, sizebits;
-                result = getArg(&address);
-                if (result) {
-                    continue;
-                }
-                result = getArg(&sizebits);
-                if (result) {
-                    continue;
-                }
-
-                sendCNode(address, sizebits);
-                putDebugChar(END);
-            }
-            break;
-            case IRQ_COMMAND: {
-                sendIRQNode();
-                putDebugChar(END);
-                result = 0;
-            }
-            break;
-            case VERSION_COMMAND: {
-                sendVersion();
-                putDebugChar(END);
-            }
-            break;
-            case DONE: {
-                done = 1;
-                putDebugChar(END);
-            }
-            default:
-                result = 0;
-                break;
-            }
-        } while (result);
     }
+    return 1;
 }
 
 #endif
